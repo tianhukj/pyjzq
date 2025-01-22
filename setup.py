a84afddacb03e6f23635714f482dbca735e9d8f8
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-22 17:16:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='pyjzq',
     version='0.1.0',
+    author="tianhukj",
+    author_email="tianhukj@outlook.com",
+    description="一个用python制作的命令行井字棋游戏",
+    long_description=open("README.md").read(),
+    long_description_content_type="game",
+    url="https://github.com/tianhukj/pyjzq",
     packages=find_packages(),
     install_requires=[
-        setuptools
+        
     ],
     entry_points={
         'console_scripts': [
