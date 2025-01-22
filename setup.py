c65ab2bdb462641d6552eb96042fd63d7ad344cb
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-22 17:36:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyjzq',
-    version='0.1.0',
-    author="tianhukj",
-    author_email="tianhukj@outlook.com",
-    description="一个用python制作的命令行井字棋游戏",
-    long_description=open("README.md").read(),
-    long_description_content_type="game",
-    url="https://github.com/tianhukj/pyjzq",
+    version='0.1',
+    author='tianhukj',
+    author_email='tianhukj@outlook.com',
+    description='A command-line tic-tac-toe game',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tianhukj/pyjzq',
     packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
     install_requires=[
-        
+        # 添加依赖包，如有需要
     ],
     entry_points={
         'console_scripts': [
