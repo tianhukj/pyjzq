3163e693d87482cdd1b39646cb3055f76ce0168e
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-22 18:03:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyjzq',
-    version='0.1.0',
+    version='0.1',
     author='tianhukj',
     author_email='tianhukj@outlook.com',
     description='A command-line tic-tac-toe game',
